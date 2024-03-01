38af798a258388c049cc9d0fc20f4639d5ec0e5b
Griego86 <dimi1086@hotmail.com>
2024-03-01 09:07:08 -0800

    Added State to components

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -1,10 +1,15 @@
-import React from "react"; // optional
+import { useState } from "react";
 
 export default function AngryButton() {
+
+  const [anger, setAnger] = useState(0);
+
   return (
-    <button className="AngryButton">
-      {/* When the threshold is not reached */}<span>Don't click me too much! </span>
-      {/* When the threshold is not reached */}<span>Rawr!</span>
+    <button className="AngryButton" style={{backgroundColor: `rgba(255,0,0,${anger})`}}>
+      {/* When the threshold is not reached */}
+      {anger < 1 && <span>Don't click me too much! </span>}
+      {/* When the threshold is not reached */}
+      {anger > 1 && <span>Rawr!</span>}
     </button>
   );
 }
