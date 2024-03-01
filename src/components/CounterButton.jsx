cb18821ddada42e8c9150540b7760f008bb77117
Griego86 <dimi1086@hotmail.com>
2024-02-29 20:42:57 -0800

    Added components and removed unnecessary css

---
+++ src/components/CounterButton.jsx
@@ -0,0 +1,9 @@
+import React from "react"; // optional
+
+export default function CounterButton () {
+  return (
+    <button className="CounterButton">
+      You clicked me X amount of times
+    </button>
+  );
+}
---
+++ src/components/TextRepeaterButton.jsx
@@ -0,0 +1,9 @@
+import React from "react"; // optional
+
+export default function TextRepeaterButton () {
+  return (
+    <button className="TextRepeaterButton">
+      <span>I like this text</span>
+    </button>
+  );
+}
